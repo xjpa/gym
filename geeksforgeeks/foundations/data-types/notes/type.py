950fc702d5dc245e1c415ec3ca6e379bf4cf1c8e
xjpa <xjpa@proton.me>
2023-11-30 23:36:13 +0800

    grind

--- geeksforgeeks/foundations/data-types/notes/type.py
+++ geeksforgeeks/foundations/data-types/notes/type.py
@@ -10,7 +10,6 @@ print(type(b)) # float
 c = 2 + 4j # complex number
 print(type(c)) # complex
 
-
 # sequence types (collections)
 
 str = "John"
@@ -18,12 +17,21 @@ print(type(str)) # str
 
 l = [10, 20, 30]
 print(type(l)) # list
+# like arrays in other languages,
+# but with python we can dynamically add/remove items
+# so its kinda like arraylist in java or vector in C++
+
 
 t = (10, 20, 30)
 print(type(t)) # tuple
+# like a list but it's immutable, we cant modify/add/remove items
 
 s = {10, 20, 30}
 print(type(s)) # set
+# curly brackets for sets
+# dynamically sized collection of distinct items
+# fast operations, internally uses hashing to implement the (distinct) keys
 
-d = {10: "gfg", 20: "ide"}
+d = {10: "a", 20: "b"}
 print(type(d)) # dict
+# dictionaries are used to store mappings
