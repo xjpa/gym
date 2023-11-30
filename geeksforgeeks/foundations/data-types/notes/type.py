0a65b58ccf443596cd301974698d86d9472569aa
xjpa <xjpa@proton.me>
2023-11-30 23:27:37 +0800

    grind

--- geeksforgeeks/foundations/data-types/notes/type.py
+++ geeksforgeeks/foundations/data-types/notes/type.py
@@ -6,3 +6,21 @@ print(type(b))
 
 c = 2 + 4j
 print(type(c))
+
+
+# sequence type
+
+str = "gfg"
+print(type(str))
+
+l = [10, 20, 30]
+print(type(l))
+
+t = (10, 20, 30)
+print(type(t))
+
+s = {10, 20, 30}
+print(type(s))
+
+d = {10: "gfg", 20: "ide"}
+print(type(d))
